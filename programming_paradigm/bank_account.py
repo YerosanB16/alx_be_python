567d8efbd3eb0d31bc4e0839d5ec4495bfb460c2
Yerosen Bekele <yerobeke@gmail.com>
2025-08-02 03:30:54 +0300

    add

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# File: bank_account.py
+
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.__account_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+
+    def withdraw(self, amount):
+        if amount > 0 and self.__account_balance >= amount:
+            self.__account_balance -= amount
+            return True
+        return False
+
+    def display_balance(self):
+        print(f"Current Balance: ${self.__account_balance}")
